3525a0e943f33ac335ec061cfeebaa82c452b020
seen2 <tiwarietc@gmail.com>
2020-09-12 13:19:17 +0530

    if else and ifelse

--- conditionStatements.sh
+++ conditionStatements.sh
@@ -40,3 +40,27 @@ then
 else 
 	echo "the condition is false"
 fi
+
+
+if [ $count -lt 10 ] #always  keep space between brackets
+then 
+	echo "the condition is true"
+
+elif [ $count -gt 10  ]
+then
+	echo "Its else if block"
+else 
+	echo "the condition is false"
+fi
+
+
+if [ $count -ne 10 ] #always  keep space between brackets
+then 
+	echo "the condition is true"
+
+elif (( $count > 10  ))
+then
+	echo "Its else if block with angle brackets"
+else 
+	echo "the condition is false"
+fi
