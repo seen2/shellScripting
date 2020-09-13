910fd8b0e5e4d6f04bef01d07b4cab977bf61129
seen2 <tiwarietc@gmail.com>
2020-09-13 13:12:05 +0530

    user inputs and command line input shell

--- userInput.sh
+++ userInput.sh
@@ -1,5 +1,25 @@
 #/bin/bash/
 
+#command line args
+#echo $0 $1 $2 $3
+
+#unknown number of command line args input
+
+#3 inputs 
+#args=("$3")
+
+args=("$@")
+
+echo ${args[0]} ${args[1]} ${args[2]} ${args[3]}
+
+#print all comand line inputs at once
+echo $@
+
+#print length of all commandline args inputs 
+
+echo $#
+
+: '
 read name
 
 echo "hello," $name
@@ -8,4 +28,4 @@ echo "thanks," $name
 
 echo $name+1
 
-
+'
